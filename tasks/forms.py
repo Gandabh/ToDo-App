7da6e1e9f7d09e0366e83d1e756cd91447022430
Gandabh <qendabhesenova@gmail.com>
2021-10-16 19:38:55 +0400

    templates modified

--- tasks/forms.py
+++ tasks/forms.py
@@ -2,8 +2,14 @@ from django import forms
 from django.db.models import fields
 from django.forms import ModelForm
 from .models import *
+from django.forms.fields import CharField
 
 class TaskForm(forms.ModelForm):
+    title = CharField(widget=forms.TextInput(attrs={'autofocus': True,
+                                                            'class': 'form-control',
+                                                            'placeholder': 'Enter your task here'
+                                                            }))
+ 
     
     class Meta:
         model=Task
